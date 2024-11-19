5533ca08ea3f59627ba5041eb025d85545410739
Christian Schwarz <cmrs@cmrs.io>
2024-11-19 15:59:53 +0100

    split up test harness

---
+++ tests/test_harness.py
@@ -0,0 +1,74 @@
+import os
+import subprocess
+import lldb  # type: ignore
+import textwrap
+from typing import Any
+
+PACKAGE_ROOT_PATH = os.path.abspath(
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        ".."
+    )
+)
+PRETTIFIER_PATH = os.path.join(
+    PACKAGE_ROOT_PATH,
+    "rust_prettifier_for_lldb.py"
+)
+
+
+def run_rust_test(
+    temp_dir: Any,
+    rust_src: str,
+    expected_var_summaries: dict[str, str]
+):
+    src_path = os.path.join(str(temp_dir), "main.rs")
+    rust_src = textwrap.indent(textwrap.dedent(rust_src), "    ")
+
+    rust_src += "    let _ = 0 + 0;"  # dummy line to place the breakpoint on
+
+    rust_src = "fn main() {\n" + rust_src + "\n}\n"
+
+    binary_path = os.path.join(str(temp_dir), "main")
+    with open(src_path, "w") as f:
+        f.write(rust_src)
+
+    result = subprocess.run(
+        ["rustc", "-g", src_path, "-o", binary_path],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+
+    if result.returncode != 0:
+        assert result.stderr.decode("utf-8") == ""
+        assert result.returncode == 0
+
+    debugger: lldb.SBDebugger = lldb.SBDebugger.Create()
+    debugger.SetAsync(False)
+
+    target: lldb.SBTarget = debugger.CreateTargetWithFileAndArch(
+        binary_path, lldb.LLDB_ARCH_DEFAULT)
+    assert target
+
+    breakpoint_line = len(rust_src.splitlines()) - 1
+    breakpoint: lldb.SBBreakpoint = target.BreakpointCreateByLocation(
+        "main.rs", breakpoint_line)
+    assert breakpoint.num_locations == 1
+
+    process = target.LaunchSimple(None, None, ".")
+    thread = process.GetThreadAtIndex(0)
+    frame = thread.GetFrameAtIndex(0)
+    assert frame
+
+    repl: lldb.SBCommandInterpreter = debugger.GetCommandInterpreter()
+    res = lldb.SBCommandReturnObject()
+    repl.HandleCommand(f"command script import {PRETTIFIER_PATH}", res)
+    assert res.Succeeded()
+
+    for (name, summary) in expected_var_summaries.items():
+        var = frame.FindVariable(name)
+        s = var.GetSummary()
+        if s is None:
+            s = var.GetValue()
+        assert s == summary
+
+    lldb.SBDebugger.Destroy(debugger)
