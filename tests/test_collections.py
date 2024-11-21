e9b54f180e1ab79f647f18a5574998e82864f31b
Christian Schwarz <cmrs@cmrs.io>
2024-11-21 22:52:03 +0100

    disable non deterministic hashmap test

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -28,14 +28,14 @@ def test_basic_vec_deque(tmpdir):
         "x": "(3) VecDeque[1, 2, 3]"
     })
 
-
-def test_hashmap_access(tmpdir):
-    src = """
-        use std::collections::HashMap;
-        use std::iter::FromIterator;
-        let hm = HashMap::<&'static str, i32>::from_iter([("foo", 3), ("bar", 12)]);
-    """
-    # TODO: seems a bit sketchy. Can we improve this?
-    expect_command_output(tmpdir, src, [
-        ("v hm[0].1", "(int) 1 = 3\n")
-    ])
+# TODO: this is non deterministic. Find better way to test this.
+# def test_hashmap_access(tmpdir):
+#    src = """
+#        use std::collections::HashMap;
+#        use std::iter::FromIterator;
+#        let hm = HashMap::<&'static str, i32>::from_iter([("foo", 3), ("bar", 12)]);
+#    """
+#    # TODO: seems a bit sketchy. Can we improve this?
+#    expect_command_output(tmpdir, src, [
+#        ("v hm[0].1", "(int) 1 = 3\n")
+#    ])
